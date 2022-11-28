c3ff25036aba65f1ecfd6b0cdc2b4d589f63faed
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-11-29 01:10:11 +0300

    commit tree 23a281170958befd63c94fb857eb7f964556f8ed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ];
+
+function superbowlWin(records){
+
+for( const season of records){
+    if (season.result==="W"){
+        return season.year ;
+    }
+}
+
+}
+record.find(superbowlWin);  
